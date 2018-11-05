6e70ece0875487feb4f7894b7bbf824fc8c36be3
areeshaFatima <areeshaf7@gmail.com>
2018-11-06 03:23:46 +0530

    menu added

--- app/src/main/java/com/example/hp/notes/MainActivity.java
+++ app/src/main/java/com/example/hp/notes/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.hp.notes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,6 +17,24 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     static ArrayList<String> notes;
+    static ArrayAdapter<String> arrayAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         ListView listView=findViewById(R.id.listView);
         notes=new ArrayList<>();
         notes.add("Example");
-        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,notes);
+        arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,notes);
 
         listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
